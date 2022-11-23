8669fef4e157e41a1afaf74dd8427137e378f64b
Anton <104712265+m6129@users.noreply.github.com>
2022-11-23 13:08:34 +0300

    Update work2.py

--- work2.py
+++ work2.py
@@ -1,42 +1,8 @@
 import io # обязательные библиотеки для stremlit
 import streamlit as st # # обязательные библиотеки для stremlit
 from PIL import Image # библиотека для загрузки изображений
-
 import torch
-import requests
-from transformers import ViTFeatureExtractor, AutoTokenizer, VisionEncoderDecoderModel
 
-
-loc = "ydshieh/vit-gpt2-coco-en"
-
-feature_extractor = ViTFeatureExtractor.from_pretrained(loc)
-tokenizer = AutoTokenizer.from_pretrained(loc)
-model = VisionEncoderDecoderModel.from_pretrained(loc)
-model.eval()
-
-
-def predict(image):
-
-    pixel_values = feature_extractor(images=image, return_tensors="pt").pixel_values
-
-    with torch.no_grad():
-        output_ids = model.generate(pixel_values, max_length=16, num_beams=4, return_dict_in_generate=True).sequences
-
-    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    preds = [pred.strip() for pred in preds]
-
-    return preds
-
-
-# We will verify our results on an image of cute cats
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-with Image.open(requests.get(url, stream=True).raw) as image:
-    preds = predict(image)
-
-print(preds)
-#@st.cache(allow_output_mutation=True)
-#def load_model():
-    #return image-to-text
 
 
     
