4b1a8dd648b19544baf1233c74e96533390ca19f
Anton <104712265+m6129@users.noreply.github.com>
2022-11-23 13:21:42 +0300

    Update work2.py

--- work2.py
+++ work2.py
@@ -5,10 +5,6 @@ import torch
 from torchvision import transforms
 import urllib
 
-model = torch.hub.load('nicolalandro/ntsnet-cub200', 'ntsnet', pretrained=True,
-                       **{'topN': 6, 'device':'cpu', 'num_classes': 200})
-
-
     
 def load_image():
     uploaded_file = st.file_uploader(label='Загрузите пожалуйста изображение') # загрузчик файлов
@@ -28,17 +24,3 @@ st.write('**Успешно3:**')
 if result: #после нажатия на которую будет запущен алгоритм...
     st.write('**Результаты распознавания:**')
     #image_to_text(img) #стоит убрать решётку и будет выдавать ошибку(пробовал по разному запускать модель и через функцию - итог один)
-    model = torch.hub.load('nicolalandro/ntsnet-cub200', 'ntsnet', pretrained=True, **{'topN': 6, 'device':'cpu', 'num_classes': 200})
-    model.eval()
-
-    url = 'https://raw.githubusercontent.com/nicolalandro/ntsnet-cub200/master/images/nts-net.png'
-    img = Image.open(urllib.request.urlopen(url))
-    scaled_img = transform_test(img)
-    torch_images = scaled_img.unsqueeze(0)
-
-    with torch.no_grad():
-        top_n_coordinates, concat_out, raw_logits, concat_logits, part_logits, top_n_index, top_n_prob = model(torch_images)
-
-        _, predict = torch.max(concat_logits, 1)
-         pred_id = predict.item()
-        print('bird class:', model.bird_classes[pred_id])
